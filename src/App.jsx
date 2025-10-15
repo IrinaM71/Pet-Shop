80f638375940baa4fdae39f45f124eeb6efb35cf
IrinaM71 <git@github.com>
2025-10-15 15:25:42 +0200

    В файле APP.jsx написала роутинг

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainLayout from "./components/layouts/MainLayout";
+import { Home } from "./pages/home/Home";
+import { Categories } from "./pages/categories/Categories";
+import { Products } from "./pages/products/Products";
+import { Sales } from "./pages/sales/Sales";
+import { Cart } from "./pages/cart/Cart";
+import { NotFound } from "./pages/notFound/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        {/*header layout*/}
+        <Route index element={<Home />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="categories/:id" element={<Categories />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<Products />} />
+        <Route path="sales" element={<Sales />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "@/components/header";
+import Footer from "../footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main style={{ flex: 0, minHeight: "50vh" }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
